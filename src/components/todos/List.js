093839f6cec57da39b4a546b4512128d845a8f95
sami616 <samuelresua@googlemail.com>
2018-07-16 16:37:30 +0100

    added posed to todo item

--- src/components/todos/List.js
+++ src/components/todos/List.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import { Todo } from './'
 import { SortableContainer } from 'react-sortable-hoc'
+import posed, { PoseGroup } from 'react-pose'
 import styled from 'styled-components'
 
 const List = props => {
   return (
     <ListBG>
-      {props.items.map((todo, index) => (
-        <Todo
-          disabled={todo.id.indexOf('temp_id') === -1 ? false : true}
-          oppositeLength={props.oppositeLength}
-          selected={props.selected}
-          toggleSelected={props.toggleSelected}
-          key={todo.id}
-          index={index}
-          todo={todo}
-          todos={props.items}
-        />
-      ))}
+      <PoseGroup>
+        {props.items.map((todo, index) => (
+          <PosedItem key={todo.id}>
+            <Todo
+              disabled={todo.id.indexOf('temp_id') === -1 ? false : true}
+              oppositeLength={props.oppositeLength}
+              selected={props.selected}
+              toggleSelected={props.toggleSelected}
+              key={todo.id}
+              index={index}
+              todo={todo}
+              todos={props.items}
+            />
+          </PosedItem>
+        ))}
+      </PoseGroup>
     </ListBG>
   )
 }
@@ -27,3 +32,17 @@ export default SortableContainer(List)
 const ListBG = styled.div`
   background: #f3f3f3;
 `
+
+const PosedItem = styled(
+  posed
+    .div
+    //   {
+    //   preEnter: { opacity: 0, x: 40 },
+    //   enter: { opacity: 1, x: 0 },
+    //   exit: {
+    //     opacity: 0,
+    //     x: 20
+    //   }
+    // }
+    ()
+)``
