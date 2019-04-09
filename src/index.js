39e42f0ab3525bf7f2c7fc418c3c7eaf967d9f85
sami616 <samuelresua@googlemail.com>
2019-04-09 21:51:55 +0100

    test form

--- src/index.js
+++ src/index.js
@@ -14,6 +14,22 @@ ReactDOM.render(
     <Router>
       <ThemeProvider theme={theme}>
         <ToasterProvider>
+          <form name="contact" netlify>
+            <p>
+              <label>
+                Name <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Email <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+
           <App />
           <Toaster />
         </ToasterProvider>
