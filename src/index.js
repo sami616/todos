67382d6c21d250d7e737f938dc44aa4fc6d29f3c
sami616 <samuelresua@googlemail.com>
2019-04-09 22:22:21 +0100

    removed form

--- src/index.js
+++ src/index.js
@@ -14,28 +14,6 @@ ReactDOM.render(
     <Router>
       <ThemeProvider theme={theme}>
         <ToasterProvider>
-          <form name="contact" method="post">
-            <input type="hidden" name="form-name" value="contact" />
-            <p>
-              <label>
-                Your Name: <input type="text" name="name" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your Email: <input type="email" name="email" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Message: <textarea name="message" />
-              </label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
-          </form>
-
           <App />
           <Toaster />
         </ToasterProvider>
